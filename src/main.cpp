b103b415f5834068aec41668a8342e1e7935a432
Theo Hallgren <theo.hallgren@gmail.com>
2026-08-27 21:52:01 -0500

    Trim opcontrol comments, move delay to loop start

--- src/main.cpp
+++ src/main.cpp
@@ -120,31 +120,20 @@ void competition_initialize() {}
 void autonomous() {}
 
 /**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
+ * Runs the  control code via Field Management System or
+ * the VEX Competition Switch, or after initialize() when
+ * not in competition mode
  */
-
 void opcontrol() {
-    // loop forever
     while (true) {
-        // get left y and right x positions
+		pros::delay(25);
+
+        // Get left y and right x positions
         int leftY = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
         int rightX = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
-        // move the robot
+        // Dual-stick arcade
         chassis.arcade(leftY, rightX);
-
-        // delay to save resources
-        pros::delay(25);
     }
 }
 #pragma endregion
